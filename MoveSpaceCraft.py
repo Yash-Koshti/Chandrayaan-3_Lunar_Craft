cbe30d2d7b23c350abd954942e9a70a5ebb1d569
Yash Koshti <133519502+Yash-Koshti@users.noreply.github.com>
2023-08-02 15:25:33 +0530

    Add files via upload
    Passing code for test of Forward and Backward.

--- MoveSpaceCraft.py
+++ MoveSpaceCraft.py
@@ -6,7 +6,7 @@ class Spacecraft:
     def Forward_and_Backward(self, commands: list):
         for c in commands:
             c = str(c).lower()
-            if c == 'f' or c == 'b':
+            if c == 'f':
                 if self.facing == 'E':
                     self.pos[0] += 1
                 elif self.facing == 'W':
@@ -19,5 +19,18 @@ class Spacecraft:
                     self.pos[2] += 1
                 if self.facing == 'D':
                     self.pos[2] -= 1
+            elif c == 'b':
+                if self.facing == 'E':
+                    self.pos[0] -= 1
+                elif self.facing == 'W':
+                    self.pos[0] += 1
+                if self.facing == 'N':
+                    self.pos[1] -= 1
+                if self.facing == 'S':
+                    self.pos[1] += 1
+                if self.facing == 'U':
+                    self.pos[2] -= 1
+                if self.facing == 'D':
+                    self.pos[2] += 1
 
         return self.pos
